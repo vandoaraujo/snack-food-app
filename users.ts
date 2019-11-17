f03b9ceb0089af8ed71d0814820947d58c2a06ca
Vando Araujo <tecladista.vando@gmail.com>
2019-11-16 22:06:58 -0300

    add user

--- users.ts
+++ users.ts
@@ -12,5 +12,6 @@ export class User {
 
 export const users: {[key:string]: User } = {
   "juliana@gmail.com": new User('juliana@gmail.com', 'Juliana', 'juliana23'),
-  "amanda@gmail.com": new User('amanda@gmail.com', 'Amanda', 'amanda21')
+  "amanda@gmail.com": new User('amanda@gmail.com', 'Amanda', 'amanda21'),
+  "matheusaraujo@gmail.com": new User('matheusaraujo@gmail.com', 'Matheusinho', 'matheus123'),
 }
